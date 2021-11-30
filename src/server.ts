0b9f88e328e3a1acd8d224ab9f15460f3812f99a
Natanael <nata.mw@hotmail.com>
2021-11-30 20:42:56 -0300

    create docs within swagger

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import expres from 'express';
 import { categoryRoutes } from './routes/category.routes';
 import { specificationRoutes } from './routes/specification.routes';
 
+import swaggerUI from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
 const app = expres();
 app.use(expres.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use('/categories', categoryRoutes);
 app.use('/specification', specificationRoutes);
