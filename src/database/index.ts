d66cf14ef26ef99cdb28bf390b2539edcfca61fc
Natanael <nata.mw@hotmail.com>
2021-12-01 22:49:01 -0300

    changes files data base and create categories table

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,13 @@
-import { createConnection, createConnections } from 'typeorm';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-createConnections();
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then(options => {
+  const newOptions = options as IOptions;
+  newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  });
+});
